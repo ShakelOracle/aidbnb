6aca7874fd981f8b6e4c4718a21088f1316552a9
netanel <netanelbarel75@gmail.com>
2018-03-05 09:40:44 +0200

    c

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,36 @@
-var hostsApp = angular.module('hosts', ["ngRoute"]);
+var hostsApp = angular.module('myApp', ["ngRoute"]);
+
+hostsApp.controller('hostsController', function ($scope) {
+
+   $scope.Houses = [{ "address":"avinadav yeroham 4 gedera"}, { "address":"magen 4 meitar"}];
+	
+    $scope.initMap = function () {
+        
+        for (var i = 0; i < $scope.Houses.length; i++) {
+        	$scope.InsertAddress($scope.Houses[i].address);
+        }
+    }
+    
+    $scope.InsertAddress = function (address) {
+    	var uluru =[];
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            "address": address
+        }, function (results) {
+            uluru = results[0].geometry.location;
+            
+            var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map,
+            icon: "/images/noBackground.png"
+        });
+        });        
+    }
+});
 
 hostsApp.config(function($routeProvider) {
     $routeProvider
@@ -14,5 +46,45 @@ hostsApp.config(function($routeProvider) {
     .when("/about", {
         templateUrl : "/views/about.html"
     })
-    .otherwise("/about");
+    .when("/benefits", {
+        templateUrl : "/views/benefits.html"
+    })
+    .when("/hosts", {
+        templateUrl : "/views/hosts.html"
+    })
+    .otherwise({
+        redirectTo: "/about"
+    });
+});
+
+hostsApp.controller('benefitsController', function ($scope) {
+
+     $scope.Houses = [{ "address":"avinadav yeroham 4 gedera"}, { "address":"magen 4 meitar"}];
+	
+    $scope.initMap = function () {
+        
+        for (var i = 0; i < $scope.Houses.length; i++) {
+        	$scope.InsertAddress($scope.Houses[i].address);
+        }
+    }
+    
+    $scope.InsertAddress = function (address) {
+    	var uluru =[];
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            "address": address
+        }, function (results) {
+            uluru = results[0].geometry.location;
+            
+            var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map,
+            icon: "/images/noBackground.png"
+        });
+        });        
+    }
 });
